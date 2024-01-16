a552ea9ad33d7c36997aa8542235b60334f4e923
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-01-16 23:46:36 +0100

    feat: basic app logic developed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,81 @@
+import { useState } from 'react'
+
 function App () {
+  const [fontSize, setFontSize] = useState(16)
+
+  const handleFontSizeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSize = parseInt(e.target.value, 10)
+    setFontSize(newSize)
+  }
+
+  const calculateLineHeight = (size: number) => {
+    const baseFontSize = 16
+    const baseLineHeight = 1.5
+    const minLineHeight = 1
+    const maxLineHeight = 2
+
+    // Fórmula inversamente proporcional ajustada con ligero acortamiento
+    const lineHeight = baseLineHeight / (size / baseFontSize) - 0.1
+
+    // Aplicamos límites superior e inferior
+    const limitedLineHeight = Math.max(
+      minLineHeight,
+      Math.min(lineHeight, maxLineHeight)
+    )
+
+    return { fontSize: size, lineHeight: limitedLineHeight }
+  }
+
+  const { fontSize: calculatedFontSize, lineHeight } =
+    calculateLineHeight(fontSize)
+
   return (
-    <div className='w-screen h-screen flex justify-center items-center bg-[#050505]'>
-      <img src='./logo.png' alt='Logo Afordin' />
-    </div>
+    <main className='w-screen h-screen flex flex-col justify-center items-center bg-cBackground'>
+      <div className='flex-1 flex flex-col justify-center items-center'>
+        <h1 className='text-[90px] font-bold bg-gradient-to-b from-[#97A8DB] via-[#EBEEF8] to-[#546391] text-transparent bg-clip-text'>
+          Perfect Line Height
+        </h1>
+        <h2 className='text-[24px] text-cTextPrimary'>
+          Know the perfect height of your lines based on your font size.
+        </h2>
+        <section className='flex mt-10 gap-x-8'>
+          <article className='w-[300px]'>
+            <div className='flex justify-between items-center font-semibold mb-4 text-[24px]'>
+              <label>
+                <p className='text-cTextPrimary'>Font Size</p>
+              </label>
+              <p className='p-2 text-cTextPrimary border-2 border-cPrimary rounded-[4px] flex items-center justify-center w-[64px] h-[40px]'>
+                {fontSize}
+              </p>
+            </div>
+            <input
+              type='range'
+              min='8'
+              max='72'
+              value={fontSize}
+              onChange={handleFontSizeChange}
+              className='w-full'
+            />
+            <div className='flex justify-between items-center font-semibold my-4 text-[24px]'>
+              <p className='text-cTextPrimary'>Line Height</p>
+              <p className='p-2 text-cTextPrimary border-2 border-cPrimary rounded-[4px] flex items-center justify-center w-[64px] h-[40px]'>
+                {lineHeight.toFixed(2)}
+              </p>
+            </div>
+          </article>
+          <article
+            className='w-[500px] text-cTextSecondary'
+            style={{ fontSize: fontSize }}
+          >
+            Lorem Ipsum is simply the filler text of the printing presses
+            template
+          </article>
+        </section>
+      </div>
+      <footer className='text-cSecondary h-[50px]'>
+        Made with ❤️ by aforcita
+      </footer>
+    </main>
   )
 }
 
--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig, presetIcons, presetWebFonts, presetUno } from 'unocss'
 
 export default defineConfig({
-  theme: {},
+  theme: {
+    colors: {
+      cBackground: '#0D1117',
+      cPrimary: '#28303F',
+      cSecondary: '#65768A',
+      cTextPrimary: '#ABB5C1',
+      cTextSecondary: '#E5ECFF'
+    }
+  },
   presets: [
     presetUno(),
     presetWebFonts({

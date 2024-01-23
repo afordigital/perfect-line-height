68e0d29d79ecf937e2f432107a3ba461833d5146
Marcos S <marcosgnasa@gmail.com>
2024-01-24 00:00:18 +0100

    fix: typography switch, text-left alignment, AligIcon's active state, types

--- src/components/common/AlignIcon.tsx
+++ src/components/common/AlignIcon.tsx
@@ -10,9 +10,9 @@ export const AlignIcon = (props: Props) => {
   return (
     <button
       onClick={onClick}
-      className={`bg-cPrimary hover:bg-[#374257] p-1 text-cTextPrimary rounded-[4px] ${
-        active && 'border-2 border-bluegray'
-      }  `}
+      className={`bg-cPrimary hover:bg-[#374257] p-1 text-cTextPrimary rounded-[4px] border-2 border-solid ${
+        active ? 'border-bluegray' : 'border-transparent'
+      }`}
     >
       {children}
     </button>
--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,8 @@ export const MODE = {
   LEFT: 'left',
   CENTER: 'center',
   RIGHT: 'right'
-}
+} as const
+export type ModeView = (typeof MODE)[keyof typeof MODE]
 
 export const MODE_VIEW_TO_ALIGNMENT = {
   [MODE.LEFT]: 'text-left',
@@ -14,4 +15,11 @@ export const TYPOGRAPHY = {
   INTER: 'inter',
   ROBOTO: 'roboto',
   MONTSERRAT: 'montserrat'
+} as const
+export type Typography = (typeof TYPOGRAPHY)[keyof typeof TYPOGRAPHY]
+
+export const TYPOGRAPHY_TO_FONT = {
+  [TYPOGRAPHY.INTER]: 'font-inter',
+  [TYPOGRAPHY.ROBOTO]: 'font-roboto',
+  [TYPOGRAPHY.MONTSERRAT]: 'font-montserrat'
 }
